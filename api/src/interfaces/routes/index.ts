ff7c30e54191a4b48f524c738b483f569578c6ca
GabrielGQA <gabrielgqa@gmail.com>
2025-08-06 08:56:49 -0300

    feat: implements middleware auth

--- api/src/interfaces/routes/index.ts
+++ api/src/interfaces/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes';
 import studentRoutes from './studentRoutes';
+import authMiddleware from '../middlewares/auth';
 
 const routes = Router();
 
 routes.use('/auth', authRoutes);
+routes.use(authMiddleware);
 routes.use('/students', studentRoutes);
 
 export default routes;
