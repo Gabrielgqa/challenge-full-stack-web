b0bd9a653d2e6c69b3fe42361853d73c2781e15a
GabrielGQA <gabrielgqa@gmail.com>
2025-08-06 05:54:08 -0300

    feat: insert required field ra

--- api/src/infrastructure/database/migrations/20250805214600_create_students.ts
+++ api/src/infrastructure/database/migrations/20250805214600_create_students.ts
@@ -2,11 +2,15 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable('students', (table) => {
+  return knex.schema.createTable('students', async (table) => {
     table.increments('id').primary();
+    table.integer('ra').unique().notNullable();
     table.string('name').notNullable();
     table.string('email').notNullable().unique();
     table.string('cpf').notNullable();
+  }).then(async () => {
+    await knex.raw(`CREATE SEQUENCE IF NOT EXISTS students_ra_seq START 2025001 OWNED BY students.ra`);
+    await knex.raw(`ALTER TABLE students ALTER COLUMN ra SET DEFAULT nextval('students_ra_seq')`);
   });
 }
 
